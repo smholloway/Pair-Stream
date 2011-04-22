b4ebf783ad771c14f7c488b6d8b5a780fa288718
Shayne Czyzewski <thelastemail@gmail.com>
2011-04-22 14:16:13 -0500

    fixed association table

--- db/schema.rb
+++ db/schema.rb
@@ -55,9 +55,4 @@ ActiveRecord::Schema.define(:version => 20110422190414) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "users_streams", :id => false, :force => true do |t|
-    t.integer "user_id"
-    t.integer "stream_id"
-  end
-
 end
