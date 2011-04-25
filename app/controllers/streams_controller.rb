a40e2c5c9909f1f4b41c68547a4df474071bb5a8
Seth Holloway <smholloway@gmail.com>
2011-04-24 21:55:38 -0500

    hooked up our mock stream view as streams show with the stream layout

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,5 +1,11 @@
 class StreamsController < ApplicationController
+  layout "application", :except => [:show]
+
   def index
     @streams = Stream.all
+  end
+
+  def show
+    render :layout => 'stream'
   end
 end
